a8140e44c90df654a4a2608b5b550c452150ba12
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2021-10-31 20:13:42 +0300

    Реализация стэка

---
+++ Homework 5/Stack/Stack/Stack.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct StackElement
+{
+    int value;
+    struct StackElement* next;
+} StackElement;
+
+void push(StackElement** head, int number)
+{
+    StackElement* newStackElement = calloc(1, sizeof(StackElement));
+    if (newStackElement == NULL)
+    {
+        return NULL;
+    }
+    newStackElement->value = number;
+    newStackElement->next = *head;
+    *head = newStackElement;
+}
+
+int pop(StackElement** head)
+{
+    if (*head == NULL || head == NULL)
+    {
+        return 0;
+    }
+    StackElement* temporary = *head;
+    int value = temporary->value;
+    *head = (*head)->next;
+    free(temporary);
+    return value;
+}
+
+int top(StackElement** head)
+{
+    if (*head == NULL || head == NULL)
+    {
+        return 0;
+    }
+    return (*head)->value;
+}
+
+bool isEmpty(StackElement* head)
+{
+    return head == NULL;
+}
+
+void deleteStack(StackElement** head)
+{
+    while (!isEmpty(*head))
+    {
+        pop(head);
+    }
+}
---
+++ Homework 5/Stack/Stack/TestsForStack.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "Stack.h"
+
+typedef struct StackElement
+{
+    int value;
+    struct StackElement* next;
+} StackElement;
+
+bool checkOfPushAndPop(StackElement** head)
+{
+    for (int i = 1; i < 11; ++i)
+    {
+        push(head, i);
+    }
+    for (int i = 10; i > 0; --i)
+    {
+        if (i != pop(head))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkOfTop(StackElement** head)
+{
+    push(head, 300);
+    return top(head) == 300;
+}
+
+bool checkOfPopFromEmptyStack(StackElement** head)
+{
+    return pop(head) == 0;
+}
+
+bool checkOfIsEmpty(StackElement** head)
+{
+    return isEmpty(*head) == true;
+}
+
+bool checkOfDeletingStack(StackElement** head)
+{
+    push(head, 67);
+    push(head, 33);
+    deleteStack(head);
+    return isEmpty(*head);
+}
+
+bool areTestsPassing(StackElement** head)
+{
+    if (!checkOfPushAndPop(head) || !checkOfPopFromEmptyStack(head) || !checkOfIsEmpty(head) 
+        || !checkOfDeletingStack(head) || !checkOfPopFromEmptyStack(head))     {
+        return false;
+    }
+    return true;
+}
