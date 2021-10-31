0ea165a306cbd5518be46ddd9cd9a70be1ef673c
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2021-10-31 20:35:41 +0300

    Задача "Постфиксный калькулятор"

---
+++ Homework 5/PostfixCalculator/PostfixCalculator/PostfixCalculator.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "C:\Users\Home\Desktop\Новая папка\Stack\Stack\TestsForStack.h"
+#include "C:\Users\Home\Desktop\Новая папка\Stack\Stack\Stack.h"
+
+int calculateInPostfixForm(char commandSequence[], StackElement** head, bool* pointerToCheckOfDivisionByZero)
+{
+    int i = 0;
+    while (commandSequence[i] != '\0')
+    {
+        int firstElement = 0;
+        int secondElement = 0;
+        int number = 0;
+        switch (commandSequence[i])
+        {
+        case ' ':
+            break;
+        case '\n':
+            break;
+        case '*':
+            firstElement = pop(head);
+            secondElement = pop(head);
+            firstElement *= secondElement;
+            push(head, firstElement);
+            break;
+        case '/':
+            firstElement = pop(head);
+            secondElement = pop(head);
+            if (firstElement == 0)
+            {
+                *pointerToCheckOfDivisionByZero = true;
+                return -1;
+            }
+            firstElement = secondElement / firstElement;
+            push(head, firstElement);
+            break;
+        case '-':
+            firstElement = pop(head);
+            secondElement = pop(head);
+            firstElement = secondElement - firstElement;
+            push(head, firstElement);
+            break;
+        case '+':
+            firstElement = pop(head);
+            secondElement = pop(head);
+            firstElement += secondElement;
+            push(head, firstElement);
+            break;
+        default:
+            number = commandSequence[i] - '0';
+            push(head, number);
+            break;
+        }
+        ++i;
+    }
+    return pop(head);
+}
+
+bool checkOfCalculation(char commandSequence[], int expectedResult)
+{
+    StackElement* head = NULL;
+    if (!areTestsPassing(&head))
+    {
+        printf("Stack's tests failed");
+        return -1;
+    }
+    bool checkOfDivisionByZero = false;
+    bool* pointerToCheckOfDivisionByZero = &checkOfDivisionByZero;
+    return calculateInPostfixForm(commandSequence, &head, pointerToCheckOfDivisionByZero) == expectedResult;
+}
+
+bool standartTest()
+{
+    char commandSequence[19] = { "9 5 * 3 / 9 1 + -" };
+    return checkOfCalculation(commandSequence, 5);
+}
+
+bool divisionByZeroTest()
+{
+    char commandSequence[19] = { "9 5 * 0 / 9 1 + -" };
+    return checkOfCalculation(commandSequence, -1);
+}
+
+bool zeroResultTest()
+{
+    char commandSequence[15] = { "3 5 - 8 + 0 *" };
+    return checkOfCalculation(commandSequence, 0);
+}
+
+int main()
+{
+    StackElement* head = NULL;
+    if (!areTestsPassing(&head))
+    {
+        printf("Stack's tests failed");
+        return -1;
+    }
+    if (!standartTest() || !divisionByZeroTest() || !zeroResultTest())
+    {
+        printf("Tests failed");
+        return -1;
+    }
+    char commandSequence[30] = { '\0' };
+    printf("Enter the sequence of digits and operations (in postfix form): ");
+    gets_s(commandSequence, 30);
+    bool checkOfDivisionByZero = false;
+    int const resultOfCalculation = calculateInPostfixForm(commandSequence, &head, &checkOfDivisionByZero);
+    if (checkOfDivisionByZero)
+    {
+        printf("\n  Incorrect form: division by zero.\n");
+        return -1;
+    }
+    printf("\nThe calculation result: %d\n", resultOfCalculation);
+}
