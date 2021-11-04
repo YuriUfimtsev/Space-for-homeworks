0211983d81d0ef5feaf43bce10dad3373b4e8ab1
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2021-11-05 02:20:20 +0300

    Добавлены тесты

--- Homework 6/Counting/Counting/Counting.c
+++ Homework 6/Counting/Counting/Counting.c
@@ -29,8 +29,46 @@ int deleteListElements(List* list, int numberOfUnluckyWarriors)
     return getValue(list, first(list));
 }
 
+bool checkOfCounting(int quantityOfWarriors, int numberOfUnluckyWarriors, int expectedNumberOfLastWarrior)
+{
+    List* cyclicList = makeListOfWarriors(quantityOfWarriors);
+    int const numberOfLastWarrior = deleteListElements(cyclicList, numberOfUnluckyWarriors);
+    deleteList(cyclicList);
+    return numberOfLastWarrior == expectedNumberOfLastWarrior;
+}
+
+bool standartTest()
+{
+    return checkOfCounting(11, 5, 8);
+}
+
+bool testWithSerialDeletion()
+{
+    return checkOfCounting(15, 1, 15);
+}
+
+bool testWithOneWarrior()
+{
+    return checkOfCounting(1, 3, 1);
+}
+
+bool testWithAFewWarriors()
+{
+    return checkOfCounting(4, 7, 2);
+}
+
+bool areTestsPassing()
+{
+    return standartTest() && testWithSerialDeletion() && testWithOneWarrior() && testWithAFewWarriors();
+}
+
 int main()
 {
+    if (!areTestsPassing())
+    {
+        printf("Tests failed");
+        return -1;
+    }
     printf("Enter the quantity of warriors and number of unlucky warriors: ");
     int quantityOfWarriors = 0;
     int numberOfUnluckyWarriors = 0;
