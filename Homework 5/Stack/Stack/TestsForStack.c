96d05eea7095104d62c5c01ca7347eaed022184b
Yuri Ufimtsev <ufimtsev3007@gmail.com>
2021-11-08 23:58:08 +0300

    After corrections

--- Homework 5/Stack/Stack/TestsForStack.c
+++ Homework 5/Stack/Stack/TestsForStack.c
@@ -3,21 +3,20 @@
 
 #include "Stack.h"
 
-typedef struct StackElement
-{
-    int value;
-    struct StackElement* next;
-} StackElement;
-
 bool checkOfPushAndPop(StackElement** head)
 {
+    bool checkOfCorrectWork = true;
     for (int i = 1; i < 11; ++i)
     {
-        push(head, i);
+        push(head, i, &checkOfCorrectWork);
+        if (!checkOfCorrectWork)
+        {
+            return false;
+        }
     }
     for (int i = 10; i > 0; --i)
     {
-        if (i != pop(head))
+        if (i != pop(head, &checkOfCorrectWork))
         {
             return false;
         }
@@ -27,33 +26,45 @@ bool checkOfPushAndPop(StackElement** head)
 
 bool checkOfTop(StackElement** head)
 {
-    push(head, 300);
-    return top(head) == 300;
+    bool checkOfCorrectWork = true;
+    push(head, 300, &checkOfCorrectWork);
+    if (!checkOfCorrectWork)
+    {
+        return false;
+    }
+    return top(head, &checkOfCorrectWork) == 300;
 }
 
 bool checkOfPopFromEmptyStack(StackElement** head)
 {
-    return pop(head) == 0;
+    bool checkOfCorrectWork = true;
+    return pop(head, &checkOfCorrectWork) == 0;
 }
 
 bool checkOfIsEmpty(StackElement** head)
 {
-    return isEmpty(*head) == true;
+    return isEmpty(*head);
 }
 
 bool checkOfDeletingStack(StackElement** head)
 {
-    push(head, 67);
-    push(head, 33);
-    deleteStack(head);
+    bool checkOfCorrectWork = true;
+    push(head, 67, &checkOfCorrectWork);
+    push(head, 33, &checkOfCorrectWork);
+    if (!checkOfCorrectWork)
+    {
+        return false;
+    }
+    deleteStack(head, &checkOfCorrectWork);
+    if (!checkOfCorrectWork)
+    {
+        return false;
+    }
     return isEmpty(*head);
 }
 
 bool areTestsPassing(StackElement** head)
 {
-    if (!checkOfPushAndPop(head) || !checkOfPopFromEmptyStack(head) || !checkOfIsEmpty(head) 
-        || !checkOfDeletingStack(head) || !checkOfPopFromEmptyStack(head))     {
-        return false;
-    }
-    return true;
+    return !checkOfPushAndPop(head) || !checkOfPopFromEmptyStack(head) || !checkOfIsEmpty(head)
+        || !checkOfDeletingStack(head) || !checkOfTop(head) ? false : true;
 }
