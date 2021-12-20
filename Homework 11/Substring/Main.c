90305be53b97f349351963b245b207630681c9ea
YuriUfimtsev <ufimtsev3007@gmail.com>
2021-12-21 02:08:37 +0300

    Initial commit. Good task without tests

---
+++ Homework 11/Substring/Main.c
@@ -0,0 +1,27 @@
+#pragma warning(disable: 4996 6031)
+
+#include <stdio.h>
+
+#include "SubstringSearch.h"
+
+int main()
+{
+    printf("Enter the pattern string (less than 50 symbols): ");
+    char patternString[51] = { '\0' };
+    scanf("%s", &patternString);
+    FILE* data = fopen("Text.txt", "r");
+    const int resultIndex = findIndexOfStringInFile(data, patternString);
+    fclose(data);
+    if (resultIndex == -1)
+    {
+        printf("\nFile doesn't contain such number of characters\n");
+    }
+    else if (resultIndex == -2)
+    {
+        printf("\nFile doesn't contain such string\n");
+    }
+    else
+    {
+        printf("\nThe index of the beginning of this string in file = %d\n", resultIndex);
+    }
+}
