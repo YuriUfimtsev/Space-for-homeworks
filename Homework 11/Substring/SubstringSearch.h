3961f5bcae97a91ef0f00e1672c3cbd761afa287
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-02-11 13:03:08 +0500

    Fix

--- Homework 11/Substring/SubstringSearch.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-
-#define MAX_SIZE_OF_STRING 255
-
-// Функция возвращает позицию первого вхождения строки patternString в файле data.
-int findIndexOfStringInFile(const char* patternString, const char* dataFromFile);
