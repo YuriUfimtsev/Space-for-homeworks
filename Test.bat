fd5e8dc9cd2f53e912e210f89ed026940bb88193
YuriUfimtsev <ufimtsev3007@gmail.com>
2022-02-13 21:26:25 +0300

    Some changes have been made

--- Test.bat
+++ Test.bat
@@ -6,7 +6,6 @@ for /R Homework8 %%i in (*.sln) do (msbuild %%i)
 for /R Homework9 %%i in (*.sln) do (msbuild %%i)
 for /R Homework10 %%i in (*.sln) do (msbuild %%i)
 for /R Homework11/Substring %%i in (*.sln) do (msbuild %%i)
-pause
 for /R Homework5 %%i in (*.exe) do (%%i tests_only & echo %%i & (IF %errorlevel%==0 (echo Tests are correct) ELSE echo Tests are INcorrect) & echo ------------)
 for /R Homework6 %%i in (*.exe) do (%%i tests_only & echo %%i & (IF %errorlevel%==0 (echo Tests are correct) ELSE echo Tests are INcorrect) & echo ------------)
 for /R Homework7 %%i in (*.exe) do (%%i tests_only & echo %%i & (IF %errorlevel%==0 (echo Tests are correct) ELSE echo Tests are INcorrect) & echo ------------)
